9789c447627d440d14b8654ad19332b147f2444d
xienaoban <22251525+xienaoban@users.noreply.github.com>
2022-08-20 17:47:50 +0800

    Empty project framework

---
+++ common/src/main/java/xienaoban/minecraft/biologydictionary/BiologyDictionary.java
@@ -0,0 +1,8 @@
+package xienaoban.minecraft.biologydictionary;
+
+public class BiologyDictionary {
+    public static final String MOD_ID = "biologydictionary";
+
+    public static void init() {
+    }
+}
