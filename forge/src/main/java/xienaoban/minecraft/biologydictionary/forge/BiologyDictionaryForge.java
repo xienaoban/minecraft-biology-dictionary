6159e6b2ecff6b55fcc83e4a552a0afab257f932
xienaoban <22251525+xienaoban@users.noreply.github.com>
2022-08-20 22:10:35 +0800

    Framework adjustment

--- forge/src/main/java/xienaoban/minecraft/biologydictionary/forge/BiologyDictionaryForge.java
+++ forge/src/main/java/xienaoban/minecraft/biologydictionary/forge/BiologyDictionaryForge.java
@@ -2,11 +2,12 @@ package xienaoban.minecraft.biologydictionary.forge;
 
 import xienaoban.minecraft.biologydictionary.BiologyDictionary;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BiologyDictionary.MOD_ID)
 public class BiologyDictionaryForge {
     public BiologyDictionaryForge() {
+        // [architectury api support]
+        // EventBuses.registerModEventBus(BiologyDictionary.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         BiologyDictionary.init();
     }
 }
