17d6915820de44cbf54b8236e595a334c6966c28
xienaoban <22251525+xienaoban@users.noreply.github.com>
2023-01-21 22:13:04 +0800

    Init project

---
+++ src/main/java/xienaoban/minecraft/biologydictionary/BiologyDictionary.java
@@ -0,0 +1,10 @@
+package xienaoban.minecraft.biologydictionary;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BiologyDictionary implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
