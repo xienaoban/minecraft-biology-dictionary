83bb2e6fa266b7c3b0fb085e6f541712950c4133
xienaoban <22251525+xienaoban@users.noreply.github.com>
2023-07-16 22:28:25 +0800

    Modify mod id

--- src/main/java/io/github/xienaoban/minecraft/biologydictionary/BiologyDictionary.java
+++ src/main/java/io/github/xienaoban/minecraft/biologydictionary/BiologyDictionary.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class BiologyDictionary {
-    public static final String MOD_ID = "biology-dictionary";
+    public static final String MOD_ID = "biologydictionary";
     public static final String MOD_KEY = "biology_dictionary";
 
     public static final String MODRINTH_PAGE = "https://modrinth.com/mod/bole";
