9e2ca04bb016f816f9d4ad975f0b5e67306fc049
xienaoban <22251525+xienaoban@users.noreply.github.com>
2023-12-31 01:03:13 +0800

    Add ASM

--- src/testUnit/java/io/github/xienaoban/minecraft/biologydictionary/VanillaEntityNbtTest.java
+++ src/testUnit/java/io/github/xienaoban/minecraft/biologydictionary/VanillaEntityNbtTest.java
@@ -1,28 +1,44 @@
 package io.github.xienaoban.minecraft.biologydictionary;
 
-import com.strobel.decompiler.Decompiler;
-import com.strobel.decompiler.PlainTextOutput;
+import io.github.xienaoban.minecraft.biologydictionary.core.EntityManager;
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.animal.Animal;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.objectweb.asm.ClassReader;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class VanillaEntityNbtTest implements FabricGameTest {
     private static final Logger LOGGER = LogManager.getLogger();
 
     @GameTest(template = EMPTY_STRUCTURE)
     public void testNbtElements(GameTestHelper helper) {
-        LOGGER.info(getDecompiledSourceCode(Entity.class));
-        LOGGER.info(getDecompiledSourceCode(Animal.class));
+        EntityManager.getInstance().dfsEntityTree(false, (cur, depth) -> {
+            ClassReader cr = getClassReader(helper, cur.getClazz());
+            if (cr == null) {
+                helper.fail("Failed to open ClassReader of " + cur.getClazz() + "(" + cur.getClazzName() + ")");
+            }
+            return true;
+        });
         helper.succeed();
     }
 
-    private static String getDecompiledSourceCode(Class<? extends Entity> clazz) {
-        PlainTextOutput output = new PlainTextOutput();
-        Decompiler.decompile(clazz.getName().replace('.', '/'), output);
-        return output.toString();
+    private static ClassReader getClassReader(GameTestHelper helper, Class<?> clazz) {
+        try {
+            String path = clazz.getName().replace('.', '/') + ".class";
+            InputStream in = Entity.class.getClassLoader().getResourceAsStream(path);
+            if (in == null) {
+                helper.fail("Entity class " + clazz + " not found.");
+                return null;
+            }
+            return new ClassReader(in);
+        } catch (IOException e) {
+            helper.fail("Cannot read entity class " + clazz + ": " + e + ".");
+            return null;
+        }
     }
 }
