1b6ee77652fa30d61511c81b19411dba990d43d2
xienaoban <22251525+xienaoban@users.noreply.github.com>
2025-07-24 01:29:25 +0800

    Add highlighting entities function

--- src/main/java/io/github/xienaoban/biologydictionary/Const.java
+++ src/main/java/io/github/xienaoban/biologydictionary/Const.java
@@ -1,5 +1,8 @@
 package io.github.xienaoban.biologydictionary;
 
 public final class Const {
+    public static final int HIGHLIGHT_ENTITIES_TICKS = 12 * 20;
+    public static final int HIGHLIGHT_ENTITIES_DISTANCE = 100;
+
     public static final int HIGHLIGHT_BLOCK_TICKS = 6 * 20;
 }
