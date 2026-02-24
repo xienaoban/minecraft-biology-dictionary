edd48c663cce5bf184f382826fd45fd554a90123
xienaoban <22251525+xienaoban@users.noreply.github.com>
2026-02-25 00:37:41 +0800

    Init architectury framework

---
+++ common/src/main/java/io/github/xienaoban/biologydictionary/Biologydictionary.java
@@ -0,0 +1,9 @@
+package io.github.xienaoban.biologydictionary;
+
+public final class Biologydictionary {
+    public static final String MOD_ID = "biologydictionary";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
--- doc/Porting.md
+++
@@ -1,6 +0,0 @@
-# Porting of New Minecraft Versions
-
-Check if the following implementations have changed:
-
-- HorseInventoryScreen#render
-  - AbstractVillagerInventoryScreen#render
