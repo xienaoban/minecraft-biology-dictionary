89390932a249f0b165e89c0c3694247a4972cfb9
xienaoban <22251525+xienaoban@users.noreply.github.com>
2026-02-26 21:02:41 +0800

    Fix access widener

---
+++ common/src/main/java/io/github/xienaoban/biologydictionary/mixin/GuiGraphicsIMixin.java
@@ -0,0 +1,15 @@
+package io.github.xienaoban.biologydictionary.mixin;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.render.state.GuiRenderState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(GuiGraphics.class)
+public interface GuiGraphicsIMixin {
+    @Accessor
+    GuiGraphics.ScissorStack getScissorStack();
+
+    @Accessor
+    GuiRenderState getGuiRenderState();
+}
