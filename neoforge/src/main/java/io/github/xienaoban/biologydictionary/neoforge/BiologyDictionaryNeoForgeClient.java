98003987620ff70875ed664d8355c6605b334147
xienaoban <22251525+xienaoban@users.noreply.github.com>
2026-03-03 23:21:28 +0800

    Improve project structure

--- neoforge/src/main/java/io/github/xienaoban/biologydictionary/neoforge/BiologyDictionaryNeoForgeClient.java
+++ neoforge/src/main/java/io/github/xienaoban/biologydictionary/neoforge/BiologyDictionaryNeoForgeClient.java
@@ -4,12 +4,14 @@ import io.github.xienaoban.biologydictionary.BiologyDictionaryClient;
 import io.github.xienaoban.biologydictionary.Lang;
 import io.github.xienaoban.biologydictionary.config.ClothConfigScreenProvider;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
+@OnlyIn(Dist.CLIENT)
 @Mod(value = Lang.BIOLOGY_DICTIONARY, dist = Dist.CLIENT)
 public class BiologyDictionaryNeoForgeClient {
     public BiologyDictionaryNeoForgeClient(IEventBus modBus, ModContainer container) {
