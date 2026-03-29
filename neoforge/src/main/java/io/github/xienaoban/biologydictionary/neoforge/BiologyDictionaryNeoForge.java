17952a9dc32889c8057c09345ae5d8b5f0a45e71
xienaoban <22251525+xienaoban@users.noreply.github.com>
2026-03-29 23:04:48 +0800

    Add entity spawn manager

--- neoforge/src/main/java/io/github/xienaoban/biologydictionary/neoforge/BiologyDictionaryNeoForge.java
+++ neoforge/src/main/java/io/github/xienaoban/biologydictionary/neoforge/BiologyDictionaryNeoForge.java
@@ -2,9 +2,7 @@ package io.github.xienaoban.biologydictionary.neoforge;
 
 import io.github.xienaoban.biologydictionary.BiologyDictionary;
 import io.github.xienaoban.biologydictionary.Lang;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod(Lang.BIOLOGY_DICTIONARY)
 public final class BiologyDictionaryNeoForge {
