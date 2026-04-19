c00a961514f419f5abe9fd4a685623c369b31778
xienaoban <22251525+xienaoban@users.noreply.github.com>
2026-04-19 22:08:01 +0800

    Port 0.8.3 from 1.21.1

--- common/src/main/java/io/github/xienaoban/biologydictionary/gui/screen/BdHomeScreen.java
+++ common/src/main/java/io/github/xienaoban/biologydictionary/gui/screen/BdHomeScreen.java
@@ -8,6 +8,7 @@ import io.github.xienaoban.biologydictionary.core.session.WorldSession;
 import io.github.xienaoban.biologydictionary.core.skill.BiologySkills;
 import io.github.xienaoban.biologydictionary.core.skill.general.GetSpawnEggSkill;
 import io.github.xienaoban.biologydictionary.core.skill.general.HighlightEntitiesSkill;
+import io.github.xienaoban.biologydictionary.gui.PlaceholderFallbackEntityRenderer;
 import io.github.xienaoban.biologydictionary.gui.component.Page;
 import io.github.xienaoban.biologydictionary.gui.component.Widget;
 import io.github.xienaoban.biologydictionary.gui.util.Colors;
@@ -170,9 +171,12 @@ public class BdHomeScreen extends AbstractBiologyDictionaryScreen {
         private final ItemStack spawnEgg;
         private final EntityType<?> entityType;
 
+        private final PlaceholderFallbackEntityRenderer entityRenderer;
+
         public EntityWidget(Entity entity) {
             super(2, 2);
             this.entity = entity;
+            this.entityRenderer = new PlaceholderFallbackEntityRenderer(entity);
             EntityType<?> type = EntityUtils.getEntityType(entity);
             this.entityType = type;
             this.name = type.getDescription();
@@ -232,7 +236,7 @@ public class BdHomeScreen extends AbstractBiologyDictionaryScreen {
             super.onRender(ctx);
             int silhouetteColor = isDiscovered() ? 0 : Colors.UNDISCOVERED_ENTITY_COLOR;
             ScreenElementBox box = getBox();
-            ctx.renderEntityCentered(entity,
+            entityRenderer.renderEntityCentered(ctx,
                     box.getLeft(), box.getTop(), box.getRight(), box.getBottom() - 6,
                     entityRotateX, entityRotateY,
                     silhouetteColor);
--- common/src/main/java/io/github/xienaoban/biologydictionary/mixin/entity/MobIMixin.java
+++ common/src/main/java/io/github/xienaoban/biologydictionary/mixin/entity/MobIMixin.java
@@ -1,12 +1,17 @@
 package io.github.xienaoban.biologydictionary.mixin.entity;
 
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(Mob.class)
 public interface MobIMixin {
     @Accessor("goalSelector")
     GoalSelector biologydictionary$getGoalSelector();
+
+    @Invoker("getAmbientSound")
+    SoundEvent biologydictionary$getAmbientSound();
 }
