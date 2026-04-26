e9f52056708f2c06aaa747c7432a3fe86145ed9d
xienaoban <22251525+xienaoban@users.noreply.github.com>
2026-04-26 20:08:46 +0800

    Fix EntityManager sort issue

--- common/src/main/java/io/github/xienaoban/biologydictionary/core/EntityManager.java
+++ common/src/main/java/io/github/xienaoban/biologydictionary/core/EntityManager.java
@@ -112,6 +112,9 @@ public final class EntityManager {
                 if (ia.getNamespace().equals(ResourceLocation.DEFAULT_NAMESPACE)) {
                     return 1;
                 }
+                if (ib.getNamespace().equals(ResourceLocation.DEFAULT_NAMESPACE)) {
+                    return -1;
+                }
                 return cmp;
             }
             String pa = ia.getPath(), pb = ib.getPath();
