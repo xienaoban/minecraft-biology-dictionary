a6d8754f95c67ce73702d794057a5bb530faa281
xienaoban <22251525+xienaoban@users.noreply.github.com>
2026-05-13 02:49:31 +0800

    Add EntitySpawnCountedWidget

--- common/src/main/java/io/github/xienaoban/biologydictionary/core/widget/EntityPropertyWidgets.java
+++ common/src/main/java/io/github/xienaoban/biologydictionary/core/widget/EntityPropertyWidgets.java
@@ -56,6 +56,7 @@ public final class EntityPropertyWidgets {
         registrar.register(WanderingTraderDespawnDelayWidget.class, WanderingTraderDespawnDelayWidget.FACTORY);
         registrar.register(TurnPagePlaceholder.TurnPage2Widget.class, TurnPagePlaceholder.TurnPage2Widget.FACTORY);
         registrar.register(EntityDiscoveryRecordWidget.class, EntityDiscoveryRecordWidget.FACTORY);
+        registrar.register(EntitySpawnCountedWidget.class, EntitySpawnCountedWidget.FACTORY);
     }
 
     private static final Map<Class<? extends Entity>, List<Entry>> registry = new HashMap<>();
