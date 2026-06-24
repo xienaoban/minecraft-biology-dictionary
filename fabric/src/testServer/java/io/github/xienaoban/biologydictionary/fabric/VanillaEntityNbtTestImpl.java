c9bc57a9cac7325331d46fec56cc7f43b5af5d43
xienaoban <22251525+xienaoban@users.noreply.github.com>
2026-06-24 20:38:29 +0800

    Port from 1.21.1

--- fabric/src/testServer/java/io/github/xienaoban/biologydictionary/fabric/VanillaEntityNbtTestImpl.java
+++ fabric/src/testServer/java/io/github/xienaoban/biologydictionary/fabric/VanillaEntityNbtTestImpl.java
@@ -1,10 +1,17 @@
 package io.github.xienaoban.biologydictionary.fabric;
 
 import io.github.xienaoban.biologydictionary.VanillaEntityNbtTest;
+import net.fabricmc.fabric.api.gametest.v1.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 
 public class VanillaEntityNbtTestImpl {
     private final VanillaEntityNbtTest test = new VanillaEntityNbtTest();
+
+    @GameTest
+    public void testEmpty(GameTestHelper helper) {
+        // Just to keep @GameTest imported.
+        helper.succeed();
+    }
 
     // @GameTest
     public void testCollectVanillaNbts(GameTestHelper helper) {
